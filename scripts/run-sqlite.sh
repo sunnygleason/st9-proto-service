469d08f2706e384c418c2c9bb3865c36a5a34783
David Phillips <david@acz.org>
2011-04-19 10:51:47 -0700

    Create logs directory

--- scripts/run-sqlite.sh
+++ scripts/run-sqlite.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+mkdir -p logs
+
 java -cp target/dependency/"*":target/"*" -Dst9.storageModule="com.g414.st9.proto.service.store.SqliteKeyValueStorage\$SqliteKeyValueStorageModule" com.g414.st9.proto.service.Main
 
