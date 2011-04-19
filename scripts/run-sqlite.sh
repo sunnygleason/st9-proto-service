c6adeefed18607193fee904a370b28240e2fe3cc
David Phillips <david@acz.org>
2011-04-19 11:03:32 -0700

    Build jar-with-dependencies

--- scripts/run-sqlite.sh
+++ scripts/run-sqlite.sh
@@ -2,5 +2,5 @@
 
 mkdir -p logs
 
-java -cp target/dependency/"*":target/"*" -Dst9.storageModule="com.g414.st9.proto.service.store.SqliteKeyValueStorage\$SqliteKeyValueStorageModule" com.g414.st9.proto.service.Main
+java -Dst9.storageModule="com.g414.st9.proto.service.store.SqliteKeyValueStorage\$SqliteKeyValueStorageModule" -jar target/st9-proto-service.jar
 
